5d05e6d6d5d09cc5177799b5e5fa6a4e71daf655
Glauco da Silva <glaucodasilva@live.com>
2018-12-17 01:39:30 -0200

    #** Seqüência de Fibonacci ** - Digite um número e peça ao programa para gerar a sequência de Fibonacci para esse número ou para o número N.

---
+++ 0002.py
@@ -0,0 +1,12 @@
+#** Seqüência de Fibonacci ** - Digite um número e peça ao programa para gerar a sequência de Fibonacci para esse número ou para o número N.
+n = int(input())
+a = 0
+b = 1
+prox = 0
+fib = '0 1'
+for i in range(1, n-1):
+    prox = a + b
+    a = b
+    b = prox
+    fib = fib + ' ' + str(prox)
+print(fib)
